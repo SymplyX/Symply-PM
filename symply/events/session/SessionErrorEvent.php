85e2c4e7957800a4444da56b4f0c8734eba47156
AzaleeX <azaleemcpe@gmail.com>
2023-12-25 23:36:55 +0100

    ⭐Clean PHP Cs Fixer

--- symply/events/session/SessionErrorEvent.php
+++ symply/events/session/SessionErrorEvent.php
@@ -35,47 +35,29 @@ use Throwable;
 class SessionErrorEvent extends Event implements Cancellable
 {
 	use CancellableTrait;
-  
-	/**
-	 * @param Player $player
-	 * @param Throwable $throwable
-	 * @param string $errorMessage
-	 */
+
 	public function __construct(
 		protected Player $player,
 		protected Throwable $throwable,
 		protected string $errorMessage
 	) {}
 
-	/**
-	 * @return Player
-	 */
-	public function getPlayer(): Player
+	public function getPlayer() : Player
 	{
 		return $this->player;
 	}
 
-	/**
-	 * @return Throwable
-	 */
-	public function getThrowable(): Throwable
+	public function getThrowable() : Throwable
 	{
 		return $this->throwable;
 	}
 
-	/**
-	 * @return string
-	 */
-	public function getErrorMessage(): string
+	public function getErrorMessage() : string
 	{
 		return $this->errorMessage;
 	}
-  
-	/**
-	 * @param string $message
-	 * @return void
-	 */
-	public function setErrorMessage(string $message): void
+
+	public function setErrorMessage(string $message) : void
 	{
 		$this->errorMessage = $message;
 	}
