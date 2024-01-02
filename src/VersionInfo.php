41cca4b8417c157931388f997903e1ca48e3ae74
Sensei Tarzan <gabrielcaptari0@gmail.com>
2024-01-02 23:14:20 +0100

    Symply 0.0.5 BETA 1

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -34,7 +34,7 @@ final class VersionInfo{
 	public const BASE_VERSION = "5.10.0";
 	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_CHANNEL = "beta";
-	public const SYMPLY_VERSION = "0.0.4";
+	public const SYMPLY_VERSION = "0.0.5";
 
 	/**
 	 * PocketMine-MP-specific version ID for world data. Used to determine what fixes need to be applied to old world
